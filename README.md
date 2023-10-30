7cef21521832c131181eea30ab8c9203c6e1d528
Patrick <tingyuchang960625@gmail.com>
2023-10-30 14:26:46 +0800

    hello world in spring boot and unit test.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learningSpringBoot
 這裡會記錄一些我學習Spring Boot時遇到的問題
+
+https://hackmd.io/@XaBBLSFiSwe6jlNbBeuWmQ/HJhch9nfT
--- src/test/java/com/patrick/example/ExampleApplicationTests.java
+++ src/test/java/com/patrick/example/ExampleApplicationTests.java
@@ -1,13 +1,34 @@
 package com.patrick.example;
 
-import org.junit.jupiter.api.Test;
+import com.patrick.example.web.HelloController;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
-class ExampleApplicationTests {
+public class ExampleApplicationTests {
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
 
 	@Test
-	void contextLoads() {
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
 	}
 
 }
